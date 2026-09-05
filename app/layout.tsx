8600fbe3024e54c3adfc10cda02e5fc62e89b837
AutosleutelRivierenland <autosleutel.rivierenland@gmail.com>
2026-09-06 01:07:12 +0200

    Compacte telefoonbutton laden

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./premium.css";
 import "./logo-fix.css";
 import "./header-tone.css";
 import "./phone-hover.css";
+import "./phone-button.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://autosleutelrivierenland.nl"),
